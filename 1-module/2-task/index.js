96c9b8e4505023fbdf93df36d21a77d39aacf4c4
pavelpolitaev <pavel.politaev@gmail.com>
2018-06-23 16:16:00 +0300

    0-module/1-task, 1-module/1-task, 1-module/2-task were done

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -12,6 +12,19 @@ function print (text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid (name) {
+
+    if(!name){
+        return false
+    }
+
+    let lengthDirtyName = name.length
+    let hasNotSpaces = (name.indexOf(' ') === -1) ? true : false
+
+    if(hasNotSpaces && lengthDirtyName > 3){
+        return true
+    }
+
+    return false
 }
 
 function sayHello () {
